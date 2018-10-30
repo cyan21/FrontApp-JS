da26ee64c342390b05709b946f8b4e2896ed5fd9
cyan21 <yann.chaysinh@gmail.com>
2018-10-30 13:00:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   componentWillMount() {
-    axios.get('http://localhost:4000/today')
+    axios.get('http://localhost:4000/today?date=20181031')
         .then(json =>  
             {
                 this.setState({
