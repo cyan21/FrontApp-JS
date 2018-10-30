3c092c30ad96a71a506fcc344948e4d93512375f
cyan21 <yann.chaysinh@gmail.com>
2018-10-30 21:52:15 +0100

    change URL for backapp

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   componentWillMount() {
-    axios.get('http://localhost:4000/today?date=20181031')
+    axios.get('http://backend:4000/today?date=20181031')
         .then(json =>  
             {
                 this.setState({
